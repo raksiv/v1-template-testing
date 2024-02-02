61dbbead7f66eb1827576f819690c57eacbae2c8
Rak Siva <rak.siva@nitric.io>
2024-02-02 10:42:54 -0700

    changing reports to use both crons and every statements

--- services/library.ts
+++ services/library.ts
@@ -2,8 +2,13 @@ import { schedule } from "@nitric/sdk";
 import { bookCreateTopic, bookRemovedTopic } from "../resources/resources";
 
 // Run every day
-schedule("inventory-report").every("24 hours", async (ctx) => {
+schedule("inventory-report-daily").every("24 hours", async (ctx) => {
   console.log(`processing at ${new Date().toLocaleString()}`);
+});
+
+// Run at 22:00 Monday through Friday.
+schedule("inventory-report-nightly").cron("0 22 * * 1-5", async (ctx) => {
+  console.log(`reminder at ${new Date().toLocaleString()}`);
 });
 
 bookCreateTopic.subscribe(async (ctx) => {
